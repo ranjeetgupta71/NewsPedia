599f48f97e45b02a36f6ed1f5aa88714f57a1fa7
ranjeetgupta71 <ranjeet.github@gmail.com>
2022-10-27 16:19:31 +0530

    check

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
+    c = John
     return (
       <div>
-        hello my
+        hello my {this.c}
       </div>
     )
   }
