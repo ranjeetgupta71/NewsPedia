d0cd3c36c15111ea842a20a55425d21a5c93737e
ranjeetgupta71 <ranjeet.github@gmail.com>
2023-03-25 00:31:21 +0530

    organize import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import News from './components/News';
+
+import './App.css';
 
 export default class App extends Component {
   render() {
