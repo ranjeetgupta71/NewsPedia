b4ae4129b5ef738934debf3c1b6458b2df1ab0a9
ranjeetgupta71 <ranjeet.github@gmail.com>
2023-04-13 21:06:58 +0530

    Adds Infinite Scroll, fixes spinner postion

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import NewsItem from './NewsItem';
 import Spinner from './Spinner';
+import InfiniteScroll from 'react-infinite-scroll-component';
 export default class News extends Component {
   static defaultProps = {
     country: 'in',
@@ -23,8 +24,9 @@ export default class News extends Component {
 
     this.state = {
       articles: [],
-      loading: false,
+      loading: true,
       page: 1,
+      totalResults: 0,
     };
     document.title = `${this.capitaliseFirstLetter(this.props.category)} - NewsPedia`;
   }
@@ -45,6 +47,19 @@ export default class News extends Component {
     // it is lifecycle method, runs after render method
     this.updateNews();
   }
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${
+      this.props.category
+    }&apiKey=9b19a7bfe25b401f883fa706fe3c5339&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults,
+    });
+  };
 
   handleNextClick = async () => {
     await this.setState({
@@ -60,43 +75,41 @@ export default class News extends Component {
   };
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1 className="text-center" style={{ margin: '33px' }}>
           NewsPedia - Top {this.capitaliseFirstLetter(this.props.category)} HeadLines
         </h1>
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title}
-                    description={element.description}
-                    imageUrl={element.urlToImage}
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
-          <div className="container d-flex justify-content-between">
-            <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePrevClick}>
-              &larr; Previous
-            </button>
-            <button
-              type="button"
-              disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)}
-              className="btn btn-dark"
-              onClick={this.handleNextClick}
-            >
-              Next&rarr;
-            </button>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+          <div className="container">
+            <div className="row">
+              {!this.state.loading &&
+                // // {this.state.articles.map((element,index) => {
+                //  return <div className="col-md-4" key = {index}></div>
+                this.state.articles.map((element, index) => {
+                  return (
+                    <div className="col-md-4" key={index}>
+                      <NewsItem
+                        title={element.title}
+                        description={element.description}
+                        imageUrl={element.urlToImage}
+                        newsUrl={element.url}
+                        author={element.author}
+                        date={element.publishedAt}
+                        source={element.source.name}
+                      />
+                    </div>
+                  );
+                })}
+            </div>
           </div>
-        </div>
-      </div>
+        </InfiniteScroll>
+      </>
     );
   }
 }
