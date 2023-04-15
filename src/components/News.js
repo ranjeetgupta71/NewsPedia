2a407f8f2405dfa587848066bd0c28575e7ade51
ranjeetgupta71 <ranjeet.github@gmail.com>
2023-04-15 23:52:28 +0530

    changes class based to function based component s completely

--- src/components/News.js
+++ src/components/News.js
@@ -1,116 +1,96 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import NewsItem from './NewsItem';
+
 import Spinner from './Spinner';
 import InfiniteScroll from 'react-infinite-scroll-component';
-export default class News extends Component {
-  static defaultProps = {
-    country: 'in',
-    pageSize: 8,
-    category: 'general',
-    apiKey: 'apiKey',
-  };
 
-  static propTypes = {
-    country: PropTypes.string,
-    pageSize: PropTypes.number,
-    category: PropTypes.string,
-  };
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  capitaliseFirstLetter = (string) => {
+  const capitaliseFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      articles: [],
-      loading: true,
-      page: 1,
-      totalResults: 0,
-    };
-    document.title = `${this.capitaliseFirstLetter(this.props.category)} - NewsPedia`;
-  }
+  // document.title = `${capitaliseFirstLetter(props.category)} - NewsPedia`;
 
-  async updateNews() {
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.setState({ loading: true });
+  const updateNews = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      totalResults: parsedData.totalResults,
-      loading: false,
-    });
-  }
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+  };
 
-  async componentDidMount() {
-    // it is lifecycle method, runs after render method
-    this.updateNews();
-  }
-  fetchMoreData = async () => {
-    this.setState({ page: this.state.page + 1 });
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${
-      this.props.apiKey
-    }&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+  useEffect(() => {
+    document.title = `${capitaliseFirstLetter(props.category)} - NewsMonkey`;
+
+    updateNews();
+    // eslint-disable-next-line
+  }, []);
+
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${
+      page + 1
+    }&pageSize=${props.pageSize}`;
+    setPage(page + 1);
 
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({
-      articles: this.state.articles.concat(parsedData.articles),
-      totalResults: parsedData.totalResults,
-    });
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
 
-  handleNextClick = async () => {
-    await this.setState({
-      page: this.state.page + 1,
-    });
-    this.updateNews();
-  };
-  handlePrevClick = async () => {
-    await this.setState({
-      page: this.state.page - 1,
-    });
-    this.updateNews();
-  };
-  render() {
-    return (
-      <>
-        <h1 className="text-center" style={{ margin: '33px' }}>
-          NewsPedia - Top {this.capitaliseFirstLetter(this.props.category)} HeadLines
-        </h1>
-        {this.state.loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={this.state.articles.length}
-          next={this.fetchMoreData}
-          hasMore={this.state.articles.length !== this.state.totalResults}
-          loader={<Spinner />}
-        >
-          <div className="container">
-            <div className="row">
-              {!this.state.loading &&
-                // // {this.state.articles.map((element,index) => {
-                //  return <div className="col-md-4" key = {index}></div>
-                this.state.articles.map((element, index) => {
-                  return (
-                    <div className="col-md-4" key={index}>
-                      <NewsItem
-                        title={element.title}
-                        description={element.description}
-                        imageUrl={element.urlToImage}
-                        newsUrl={element.url}
-                        author={element.author}
-                        date={element.publishedAt}
-                        source={element.source.name}
-                      />
-                    </div>
-                  );
-                })}
-            </div>
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: '33px' }}>
+        NewsPedia - Top {capitaliseFirstLetter(props.category)} HeadLines
+      </h1>
+      {loading && <Spinner />}
+      <InfiniteScroll dataLength={articles.length} next={fetchMoreData} hasMore={articles.length !== totalResults} loader={<Spinner />}>
+        <div className="container">
+          <div className="row">
+            {!loading &&
+              // // {articles.map((element,index) => {
+              //  return <div className="col-md-4" key = {index}></div>
+              articles.map((element, index) => {
+                return (
+                  <div className="col-md-4" key={index}>
+                    <NewsItem
+                      title={element.title}
+                      description={element.description}
+                      imageUrl={element.urlToImage}
+                      newsUrl={element.url}
+                      author={element.author}
+                      date={element.publishedAt}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })}
           </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-}
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 8,
+  category: 'general',
+  apiKey: 'apiKey',
+};
+
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,39 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
 
-    return (
-      <div className="my-3">
-        <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '92%', zIndex: 1 }}>
-            {source}
-          </span>
-          <img
-            className="card-img-top"
-            src={
-              imageUrl
-                ? imageUrl
-                : 'https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/8C5A/production/_127603953_gettyimages-1244632300-1.jpg'
-            }
-            alt="Card imageCap"
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
+  return (
+    <div className="my-3">
+      <div className="card">
+        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '92%', zIndex: 1 }}>
+          {source}
+        </span>
+        <img
+          className="card-img-top"
+          src={
+            imageUrl
+              ? imageUrl
+              : 'https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/8C5A/production/_127603953_gettyimages-1244632300-1.jpg'
+          }
+          alt="Card imageCap"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
 
-            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
-              Read More
-            </a>
-            <p className="card-text">
-              <small className="text-body-secondary">
-                By {author ? author : 'unknown'} on {new Date(date).toGMTString()}
-              </small>
-            </p>
-          </div>
+          <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+            Read More
+          </a>
+          <p className="card-text">
+            <small className="text-body-secondary">
+              By {author ? author : 'unknown'} on {new Date(date).toGMTString()}
+            </small>
+          </p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+export default NewsItem;
